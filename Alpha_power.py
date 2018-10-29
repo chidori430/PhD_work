4827314536929c20dc87a7240169fbbd7a6cb7db
chidori430 <37119143+chidori430@users.noreply.github.com>
2018-10-28 22:34:51 -0500

    Add files via upload

---
+++ Alpha_power.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def length(L_base,V_base,V_threshold,voltage,alpha):
+    
+    return pipe_length
+ 
+def num_of_pipes(V_base,V_threshold,voltage,alpha):
+    
+    return n
+
+def max_frequency(voltage,V_threshold):
+    
+    return freq
+
+def alpha_power(Dyn_Pwr_base,voltage,frequency,V_base,Static_Pwr_base,num_of_pipes,Dyn_Pwr_latch,Static_pwr_latch):
+    
+    return a_power
